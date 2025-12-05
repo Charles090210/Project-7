a489839e97be33dad3c221ae2d5c6357ffb68488
Charles Piao <Charles@Ladmin25s-iMac.local>
2025-12-05 09:35:57 -0600

    First Version

---
+++ Scripts/player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody3D
+
+@export var health : int = 3
+@export var move_speed : float = 5.0
+@export var jump_force : float = 8.0
+@export var gravity : float = 20.0
+
+@onready var camera : Camera3D = $Camera3D
+
+func _physics_process(delta: float) -> void:
+	
+	if global_position.y < -5:
+		_game_over()
+	# Apply gravity
+	velocity.y -= gravity * delta
+	#Implement jumping
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jump_force
+	#Implement movement
+	var move_input : Vector2=Input.get_vector("right","left","backward","forward")
+	var move_dir : Vector3 = Vector3(move_input.x,0,move_input.y)
+	velocity.x=move_dir.x*move_speed
+	velocity.z=move_dir.z*move_speed
+	
+	move_and_slide()
+	
+func take_damage(amount: int):
+	health -= amount
+	if health <= 0:
+		_game_over()
+		
+func _game_over():
+	PlayerStats.score = 0
+	#get_tree().change_scene_to_file("res://Scene/Menu.tscn")
+	get_tree().reload_current_scene()
+
+func increase_score(amount: int):
+	PlayerStats.score += amount
+	print(PlayerStats.score)
